9eac779a60eecb82848988d377eed0deb5306df2
WoolDoughnut310 <wooldoughnutspi@outlook.com>
2023-10-29 13:05:50 +0000

    Actualizar sound.ts

--- src/lib/sound.ts
+++ src/lib/sound.ts
@@ -1,3 +1,19 @@
-export function getSounds() {
+import bell from "bell.mp3";
+import ring from "ring.wav";
+import scifiBeep from "scifi-beep.wav";
 
+export const sounds = {
+    bell,
+    ring,
+    scifiBeep
+};
+
+export const playSound = (sound: string) => {
+    if (!sounds.includes(sound)) return;
+
+    const audio = new Audio(sounds[sound]);
+  
+    audio.addEventListener("canplaythrough", () => {
+    audio.play();
+    });
 }
