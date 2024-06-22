2ffa2ddc056be5616d08509397a998a4b872d7f9
WoolDoughnut310 <wooldoughnutspi@outlook.com>
2024-06-22 18:59:31 +0100

    Actualizar vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-    plugins: [sveltekit(), VitePWA({ registerType: 'autoUpdate', devOptions: {enabled: true} })]
+    plugins: [sveltekit(), VitePWA({ registerType: 'autoUpdate', devOptions: { enabled: true } })]
 });
