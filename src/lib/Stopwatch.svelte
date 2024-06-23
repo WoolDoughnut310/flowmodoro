47a4373fdde7fdeeddda8d1067fb428815054e57
WoolDoughnut310 <wooldoughnutspi@outlook.com>
2024-06-23 12:55:41 +0100

    Actualizar Stopwatch.svelte

--- src/lib/Stopwatch.svelte
+++ src/lib/Stopwatch.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <div
-	class="group relative rounded-2xl border-4 container px-8 py-12 flex justify-center items-center"
+	class="group relative rounded-2xl border-4 sm:container px-4 py-6 sm:px-8 sm:py-12 flex justify-center items-center"
 >
 	<TimeDisplay />
 	<StopwatchOverlay />
