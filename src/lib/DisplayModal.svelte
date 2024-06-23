86fab4e802839e10c4ea9a3af79c5fd388529de1
WoolDoughnut310 <wooldoughnutspi@outlook.com>
2024-06-23 20:37:37 +0100

    changed colour theme to dark

--- src/lib/DisplayModal.svelte
+++ src/lib/DisplayModal.svelte
@@ -33,7 +33,7 @@
 </script>
 
 <dialog class="modal !ml-0 sm:!ml-4" bind:this={dialogElement}>
-	<div class="modal-box w-full h-full sm:h-3/5 max-w-4xl max-h-full rounded-none sm:rounded-xl">
+	<div class="modal-box w-full h-full sm:h-3/5 max-w-4xl max-h-full rounded-none sm:rounded-xl bg-gray-800">
 		<header class="sticky px-8 w-full flex flex-col sm:flex-row space-y-4 sm:space-y-0 justify-between items-center">
 			<h2 class="font-bold text-4xl">{title}</h2>
 			<slot name="header" />
