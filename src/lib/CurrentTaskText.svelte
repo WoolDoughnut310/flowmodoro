43eda110585fceb1fbc1679d3b1738984fce2d20
WoolDoughnut310 <wooldoughnutspi@outlook.com>
2024-06-23 13:16:33 +0100

    Actualizar CurrentTaskText.svelte

--- src/lib/CurrentTaskText.svelte
+++ src/lib/CurrentTaskText.svelte
@@ -6,7 +6,12 @@
 </script>
 
 <div class="flex flex-row justify-center items-center">
+<div class="hidden sm:contents">
 	<ClipboardIcon size="64" />
+</div>
+<div class="contents sm:hidden">
+<ClipboardIcon size="32" />
+</div>
 	<p class="ml-4 text-lg sm:text-4xl font-mono">
 		{currentTask ? currentTask.name : 'No task selected'}
 	</p>
